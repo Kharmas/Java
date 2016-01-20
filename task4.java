5519f82b0664244c6e47ad7cae6f60247c10c029
Kharmas <kharmas14@gmail.com>
2016-01-20 17:33:47 +0200

    task4

---
+++ task4.java
@@ -0,0 +1,16 @@
+package com.javacore;
+
+//Для заданого рядка і символа дати відповідь:
+// чи зустрічається цей символ у рядку
+public class task4 {
+
+    public static void main(String[] args) {
+
+        String s = "qazxsnwednftgn";
+        boolean findn = s.contains("n");
+
+        System.out.println(findn);
+
+    }
+
+}
